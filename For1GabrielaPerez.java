acb5a00bed6ecc6bee98f72e13661682890ede77
Ela-byte-cyber <gabrielaperezcastellanos@gmail.com>
2025-04-02 08:32:12 -0500

    forGabrielaPérez

---
+++ For1GabrielaPerez.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.for1gabrielaperez;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class For1GabrielaPerez {
+
+    public static void main(String[] args) {
+        String colores[] = {"morado", "rosado", "azul", "verde", "negro", "blanco"};
+        
+        for (int i=0; i < colores.length; i++){
+            System.out.println(colores[i]);
+        }
+    }
+}
